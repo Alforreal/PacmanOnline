87bea2ce3910fff08573e63256e616ec82f40ef7
Alex <aleando06@gmail.com>
2023-05-01 17:52:23 +0200

    Cleaning up comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "Headers/Shader.h"
-// #include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
 #include <stdlib.h>
 // #include <glm/ext/matrix_clip_space.hpp> // previously didn't work without them, now it does, idk why
 // #include <glm/ext/matrix_transform.hpp> //required for matrix transformation, or at least I thought so, code works without them
@@ -13,66 +14,47 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include "Headers/Collision.h"
+#include "Headers/Classes.h"
 // #include <unistd.h> // used for usleep()
 
 const int WINDOW_HEIGHT = 600, WINDOW_WIDTH = 600;
-bool pmovement = false; // movement on a previous iteration, not yet implemented
-class Sprite // used for the Pacman and the ghosts(hopefully)
-{
-    public:
-        float size;
-        float speed;
-        bool movement = false;
-        glm::vec4 color; // Color of the Pacman     
-        float pos[18]; // used for the structure of the sprite for opengl(in the form of triangles)
-        unsigned int VBO;
-        unsigned int VAO;
-        glm::mat4 view = glm::mat4(1.0f); // view matrix for manipulating the movement
-        glm::mat4 model = glm::mat4(1.0f); // model matrix for opengl
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f, 100.0f); // projection matrix for the perspective
-};
-Sprite Pacman;
-class wall
-{
-    public:
-        float width[128];
-        float height[128];
-        float pos[18];
-        glm::vec4 color; // Color of the walls
-        float coords[256];
-        unsigned int VBO;
-        unsigned int VAO;
-        glm::mat4 view = glm::mat4(1.0f); // view matrix
-        glm::mat4 model = glm::mat4(1.0f); // model matrix
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f, 100.0f);
-};
+std::string map;
+// bool pmovement = false; // movement on a previous iteration, not yet implemented
+sprite Pacman;
 wall Wall;
-
-// Button class(WIP):
-struct Button {
-    float height;
-    float width;
-    bool isPressed;
-};
-int timeout = 0; // used for a timeout for collision, not yet implemented
+button OriginalMapButton;
+button TestButton;
+glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f, 100.0f);
 
 void LogMovement(float x, float y);
-void processInput(GLFWwindow *window);
+void processGameInput(GLFWwindow *window);
+void processMenuInput(GLFWwindow *window);
 int main()
 {
     // Filling the classes with information:
         // Pacman:
-    Pacman.size = 0.05f;
-    Pacman.speed = 0.015f;
+    Pacman.size = 0.03f;
+    Pacman.speed = 0.01f;
     Pacman.color = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
-    Pacman.pos[0]  =  Pacman.size; Pacman.pos[1]  =  Pacman.size; Pacman.pos[2]  = 0.0f;
-    Pacman.pos[3]  =  Pacman.size; Pacman.pos[4]  = -Pacman.size; Pacman.pos[5]  = 0.0f;
-    Pacman.pos[6]  = -Pacman.size; Pacman.pos[7]  =  Pacman.size; Pacman.pos[8]  = 0.0f;
-    Pacman.pos[9]  = -Pacman.size; Pacman.pos[10] =  Pacman.size; Pacman.pos[11] = 0.0f;
-    Pacman.pos[12] = -Pacman.size; Pacman.pos[13] = -Pacman.size; Pacman.pos[14] = 0.0f;
-    Pacman.pos[15] =  Pacman.size; Pacman.pos[16] = -Pacman.size; Pacman.pos[17] = 0.0f;
+    Pacman.findpos();
         // Walls:
     Wall.color = glm::vec4(0.0f, 0.0f, 1.0f, 1.0f);
+        // Buttons:
+    OriginalMapButton.width = 0.5f;
+    OriginalMapButton.height = 0.1f;
+    OriginalMapButton.coords[0] = 0.5f;
+    OriginalMapButton.coords[1] = 0.0f;
+    OriginalMapButton.color = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    OriginalMapButton.findpos();
+    OriginalMapButton.isPressed = false;
+
+    TestButton.width = 0.5f;
+    TestButton.height = 0.1f;
+    TestButton.coords[0] = -0.5f;
+    TestButton.coords[1] = 0.0f;
+    TestButton.color = glm::vec4(1.0f, 1.0f, 0.0f, 1.0f);
+    TestButton.findpos();
+    TestButton.isPressed = false;
 
     // initializing glfw:
     glfwInit();
@@ -81,7 +63,6 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Menu screen:
-    /*
     GLFWwindow* menu = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Menu screen", NULL, NULL);
     if(menu == NULL) // error checking with glfw
     {
@@ -90,7 +71,6 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(menu);
-
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Error: GLAD initiation failed <window type: menu>\n";
@@ -98,8 +78,74 @@ int main()
         return -1;
     }
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-    */
+    // Buttons config:
+    Shader OriginalMapButtonShader("Shaders/Buttons/OriginalMapBtn.vs", "Shaders/Buttons/OriginalMapBtn.fs");
+    glGenVertexArrays(1, &OriginalMapButton.VAO);
+    glGenBuffers(1, &OriginalMapButton.VBO);
+    glBindVertexArray(OriginalMapButton.VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, OriginalMapButton.VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(OriginalMapButton.pos), OriginalMapButton.pos, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    OriginalMapButton.view = translate(OriginalMapButton.view, glm::vec3(OriginalMapButton.coords[0], OriginalMapButton.coords[1], -2.0f));
+    OriginalMapButton.projection = projection;
+    
+    Shader TestMapButtonShader("Shaders/Buttons/TestMapBtn.vs", "Shaders/Buttons/TestMapBtn.fs");
+    glGenVertexArrays(1, &TestButton.VAO);
+    glGenBuffers(1, &TestButton.VBO);
+    glBindVertexArray(TestButton.VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, TestButton.VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(TestButton.pos), TestButton.pos, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    TestButton.view = translate(TestButton.view, glm::vec3(TestButton.coords[0], TestButton.coords[1], -2.0f));
+    TestButton.projection = projection;
 
+    while(!glfwWindowShouldClose(menu))
+    {
+        processMenuInput(menu);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // render the triangle
+        OriginalMapButtonShader.use();
+        glBindVertexArray(OriginalMapButton.VAO);
+        // uniform values for the buttons:
+        unsigned int ButtonModelLoc = glGetUniformLocation(OriginalMapButtonShader.ID, "Bmodel");
+        unsigned int ButtonViewLoc = glGetUniformLocation(OriginalMapButtonShader.ID, "Bview");
+        unsigned int ButtonColorLoc = glGetUniformLocation(OriginalMapButtonShader.ID, "BColor");
+        unsigned int ButtonProjectionLoc = glGetUniformLocation(OriginalMapButtonShader.ID, "Bprojection");
+        glUniformMatrix4fv(ButtonModelLoc, 1, GL_FALSE, glm::value_ptr(OriginalMapButton.model));
+        glUniformMatrix4fv(ButtonViewLoc, 1, GL_FALSE, glm::value_ptr(OriginalMapButton.view));
+        glUniform4fv(ButtonColorLoc, 1, glm::value_ptr(OriginalMapButton.color));
+        glUniformMatrix4fv(ButtonProjectionLoc, 1, GL_FALSE, glm::value_ptr(OriginalMapButton.projection));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
+        TestMapButtonShader.use();
+        ButtonModelLoc = glGetUniformLocation(TestMapButtonShader.ID, "Bmodel");
+        ButtonViewLoc = glGetUniformLocation(TestMapButtonShader.ID, "Bview");
+        ButtonColorLoc = glGetUniformLocation(TestMapButtonShader.ID, "BColor");
+        ButtonProjectionLoc = glGetUniformLocation(TestMapButtonShader.ID, "Bprojection");
+        glUniformMatrix4fv(ButtonModelLoc, 1, GL_FALSE, glm::value_ptr(TestButton.model));
+        glUniformMatrix4fv(ButtonViewLoc, 1, GL_FALSE, glm::value_ptr(TestButton.view));
+        glUniform4fv(ButtonColorLoc, 1, glm::value_ptr(TestButton.color));
+        glUniformMatrix4fv(ButtonProjectionLoc, 1, GL_FALSE, glm::value_ptr(TestButton.projection));
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        glfwSwapBuffers(menu);
+        glfwPollEvents();
+        if(OriginalMapButton.isPressed)
+        {
+            map = "Maps/Original.lvl";
+            glfwDestroyWindow(menu);
+            break;
+        }
+        if(TestButton.isPressed)
+        {
+            map = "Maps/TestPlayground.lvl";
+            glfwDestroyWindow(menu);
+            break;
+        }
+    }
     // Configuring the main game:
     GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "test", NULL, NULL);
     if(window == NULL)
@@ -127,9 +173,9 @@ int main()
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-
     // mathematics:
     Pacman.view = translate(Pacman.view, glm::vec3(0.0f, 0.0f, -2.0f));
+    Pacman.projection = projection; 
     
     // Wall config:
     Shader WallShader("Shaders/Wall.vs", "Shaders/Wall.fs");    
@@ -141,13 +187,14 @@ int main()
     glEnableVertexAttribArray(0);
     // mathematics for the wall
     Wall.model = glm::translate(Wall.model, glm::vec3(0.0f, 0.0f, -2.0f));
+    Wall.projection = projection;
 
     // render loop
     while (!glfwWindowShouldClose(window))
     {
         // input
         Pacman.movement = true;
-        processInput(window);
+        processGameInput(window);
         // render
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -172,7 +219,7 @@ int main()
         glBindVertexArray(Wall.VAO);
         // loading data from the .lvl file:
         std::ifstream level;
-        level.open("Maps/Square.lvl");
+        level.open(map.c_str());
         if(level)
         {       
             int i = 0, j = 0;
@@ -230,7 +277,7 @@ int main()
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
 int collision;
-void processInput(GLFWwindow *window)
+void processGameInput(GLFWwindow *window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
@@ -349,4 +396,17 @@ void LogMovement(float x, float y)
 {
     system("clear");
     std::cout << "Position(x, y): " << x << ", " << y << "\n";
+}
+void processMenuInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    double mx, my;
+    glfwGetCursorPos(window, &mx, &my);
+    // LogMovement(mx, my);
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS)
+    {
+        OriginalMapButton.isPressed = MouseDetection((float)mx/((float)WINDOW_WIDTH/2)-1.0f, (float)my/((float)WINDOW_HEIGHT/2)-1.0f, OriginalMapButton.coords[0], OriginalMapButton.coords[1], OriginalMapButton.width, OriginalMapButton.height);
+        TestButton.isPressed = MouseDetection((float)mx/((float)WINDOW_WIDTH/2)-1.0f, (float)my/((float)WINDOW_HEIGHT/2)-1.0f, TestButton.coords[0], TestButton.coords[1], TestButton.width, TestButton.height);
+    }
 }
